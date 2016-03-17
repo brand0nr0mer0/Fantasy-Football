413ca2eabbbfa3fa68e5636cd4fc1f86b3c83634
Brandon Romero <brandon@beltranfamily.com>
2016-03-16 17:53:08 -0700

    Parses player links from single page

--- FootballWebsiteDataScripts/nflplayers.py
+++ FootballWebsiteDataScripts/nflplayers.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
 import xml.etree.ElementTree
 from string import ascii_lowercase
 import os
@@ -20,14 +21,26 @@ def GetPlayerUrl(xmltree):
     return base + extension
 
 def AssembleListOfAllPlayers(url):
-    ##for c in ascii_lowercase:
+    ##for c in ascii_lowercase: need to iterate through all letters
+    ## for each letter then iterate through all pages
     address = url.format('a', 1)
     r = requests.get(address)
 
-    #print r.text
-    soup = BeautifulSoup(r.text, "html.parser")
-    for link in soup.find_all('tr'):
-        print type(link.a)
+
+    soup = BeautifulSoup(r.text, "html.parser", parse_only=SoupStrainer('table', 'statistics scrollable'), from_encoding='UTF-8')
+    links = soup.findAll('a')
+
+    playerList = []
+    for link in links:
+        href = link.get('href').encode('ascii', 'ignore')
+        playerName = link.text.encode('ascii', 'ignore')
+        tup = (href, playerName)
+        playerList.append(tup)
+
+    for row in playerList:
+        print row
+
+
 
 
 if __name__ == "__main__":
