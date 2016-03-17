44644dc82c1821c074eb0a27782c87971c1e1f95
Brandon Romero <brandon@beltranfamily.com>
2016-03-16 22:49:51 -0700

    Script collects all player names and urls

--- FootballWebsiteDataScripts/nflplayers.py
+++ FootballWebsiteDataScripts/nflplayers.py
@@ -3,8 +3,12 @@ from bs4 import SoupStrainer
 import xml.etree.ElementTree
 from string import ascii_lowercase
 import os
-import urllib2
 import requests
+import csv
+import numpy as np
+import pandas as pd
+
+
 
 playersurl = ""
 
@@ -21,26 +25,46 @@ def GetPlayerUrl(xmltree):
     return base + extension
 
 def AssembleListOfAllPlayers(url):
-    ##for c in ascii_lowercase: need to iterate through all letters
-    ## for each letter then iterate through all pages
-    address = url.format('a', 1)
+
+    dataframe = pd.DataFrame(columns=['url', 'lastName', 'firstName'])
+    for c in ascii_lowercase:
+        page = 1
+        while True:
+            address = url.format(c, page)
+
+            links = ExtractLinks(address)
+            if not links: break
+            for link in links:
+                href = link.get('href').encode('ascii', 'ignore')
+                playerName = link.text.encode('ascii', 'ignore')
+                firstName, lastName = ParsePlayerName(playerName)
+
+                if firstName and lastName:
+                    dataframe = dataframe.append(pd.DataFrame({'url' : [href], 'lastName' : [lastName], 'firstName' : [firstName]}))
+            page += 1
+
+        print c + ' | page: ' + str(page)
+
+
+    dataframe.to_csv('PlayerUrls.csv', sep=',')
+
+
+def ParsePlayerName(playerName):
+    nameSplit = playerName.split(',')
+    if len(nameSplit) > 2: raise ValueError('Name has more than 2 values' + nameSplit)
+    if len(nameSplit) == 2: ##log somehow for names that have 1 name or more than 2
+        lastName = nameSplit[0]
+        firstName = nameSplit[1].strip()
+        return firstName, lastName
+
+    return None, None
+
+def ExtractLinks(address):
     r = requests.get(address)
-
-
-    soup = BeautifulSoup(r.text, "html.parser", parse_only=SoupStrainer('table', 'statistics scrollable'), from_encoding='UTF-8')
+    soup = BeautifulSoup(r.text, "html.parser", parse_only=SoupStrainer('table', 'statistics scrollable'),
+                         from_encoding='UTF-8')
     links = soup.findAll('a')
-
-    playerList = []
-    for link in links:
-        href = link.get('href').encode('ascii', 'ignore')
-        playerName = link.text.encode('ascii', 'ignore')
-        tup = (href, playerName)
-        playerList.append(tup)
-
-    for row in playerList:
-        print row
-
-
+    return links
 
 
 if __name__ == "__main__":
